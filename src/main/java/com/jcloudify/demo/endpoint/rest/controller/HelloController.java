2a1b9fd71f3a5529a667acc3af080fdf3f13de08
MiiJack <f.rama723@gmail.com>
2025-02-04 19:16:23 +0300

    feat: Hello Controller

---
+++ src/main/java/com/jcloudify/demo/endpoint/rest/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.jcloudify.demo.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class HelloController {
+
+  public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+  public static final ResponseEntity<String> KO =
+      new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+
+  @GetMapping("/hello")
+  public String ping() {
+    return "Hello World";
+  }
+}
